1c5b85a05666211fc6d8517b848c84f5aa7f4757
sergey <bass89@mail.ru>
2023-10-18 00:50:07 +0300

    typescript build false

--- client/next.config.js
+++ client/next.config.js
@@ -7,7 +7,14 @@ const nextConfig = {
     },
     images: {
         dangerouslyAllowSVG: true,
-    }
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 }
 
 module.exports = nextConfig
