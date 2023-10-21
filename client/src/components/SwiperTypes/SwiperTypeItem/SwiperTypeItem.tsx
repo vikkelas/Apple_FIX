03b34c4b283467f5b0198e246bb24bcb6515398f
sergey <bass89@mail.ru>
2023-10-22 00:08:05 +0300

    version: 1.2 - fix swiper type logic icon

--- client/src/components/SwiperTypes/SwiperTypeItem/SwiperTypeItem.tsx
+++ client/src/components/SwiperTypes/SwiperTypeItem/SwiperTypeItem.tsx
@@ -6,16 +6,29 @@ import style from './SwiperTypeItem.module.sass'
 import Link from "next/link";
 import {useRouter} from "next/router";
 import clsx from "clsx";
+import Image from "next/image";
 
 const SwiperTypeItem:React.FC<{type:ResponseTypesI}> = ({type}) => {
     const router = useRouter();
     const typeSlug = router.query.type;
+    const iconMenu = () => {
+        return Object.keys(SvgList).findIndex(i=>i===type.icon_name)!== -1;
+    }
     return (
         <Link
             href={`/shop/${type.slug}`}
             className={clsx([style.item, typeSlug===type.slug&&'active_menu'])}>
             <div>
-                <IconSVG height={24} width={24} imageId={SvgList[type.icon_name]}/>
+                {iconMenu?
+                    <IconSVG height={24} width={24} imageId={SvgList[type.icon_name]}/>:
+                    <Image
+                        alt={type.icon_name}
+                        src={`/icons/svg/${type.icon_name}.svg`}
+                        height={24}
+                        width={24}
+                    />
+                }
+
             </div>
             <p>{type.title}</p>
         </Link>
